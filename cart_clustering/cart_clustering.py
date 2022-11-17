6ac6cc776083cce4f0f726fc15caab5ae9c19d0f
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-11-17 15:11:06 +0100

    Add generate models and data viz

---
+++ cart_clustering/cart_clustering.py
@@ -0,0 +1,50 @@
+# Import all necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import SpectralClustering
+import pickle
+from sklearn.decomposition import TruncatedSVD
+
+# Define carts : a ticket contains one or more libelles
+carts = pd.read_csv("KaDo.csv", usecols=["TICKET_ID", "LIBELLE"])
+
+# Get a list of all unique tickets
+tickets = np.unique(carts["TICKET_ID"])
+
+# Apply fonction using list of unique tickets and the carts_endoded df
+tickets_items_list = pickle.load(open("items_list_for_ticket.txt", "rb"))
+
+# Vectorize each string and return all vectors and feature names
+def prepSparseMatrix(list_of_str):
+    cv = CountVectorizer(token_pattern=r"[^\,\ ]+", lowercase=False)
+    sparseMatrix = cv.fit_transform(list_of_str)
+    return sparseMatrix, cv.get_feature_names_out()
+
+
+sparseMatrix, feature_names = prepSparseMatrix(tickets_items_list)
+svd = TruncatedSVD()
+
+for i in range(0, 5):
+    df_svd = svd.fit_transform(sparseMatrix[i * 10000 : (i + 1) * 10000])
+
+    # Fitting Data on Model and saving it with pickle (kmeans)
+    kmeans = KMeans(
+        n_clusters=8, init="k-means++", max_iter=300, n_init=10, random_state=0
+    )
+    kmeans.fit(df_svd)
+    pickle.dump(kmeans, open(f"models/kmeans/{i}.pkl", "wb"))
+
+    # Fitting Data on Model and saving it with pickle (dbscan)
+    dbscan = DBSCAN(eps=0.2, min_samples=5, algorithm="ball_tree")
+    dbscan.fit(df_svd)
+    pickle.dump(dbscan, open(f"models/dbscan/{i}.pkl", "wb"))
+
+    # Fitting Data on Model and saving it with pickle (spectral)
+    spectral = SpectralClustering(
+        n_clusters=8, assign_labels="discretize", random_state=0
+    )
+    spectral.fit(df_svd)
+    pickle.dump(spectral, open(f"models/spectral/{i}.pkl", "wb"))
